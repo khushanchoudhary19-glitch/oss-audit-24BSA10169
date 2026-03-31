91eaa51f0907f7da78fd9bf99f84ec8282da5b7a
khushanchoudhary19-glitch <khushanchoudhary19@gmail.com>
2026-03-31 23:43:38 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -3,7 +3,7 @@
 # Author: Prathamesh Kumar | Course: Open Source Software
 
 # --- Variables ---
-STUDENT_NAME="Prathamesh Kumar"
+STUDENT_NAME="Khushan Choudhary"
 SOFTWARE_CHOICE="Visual Studio Code"
 
 # --- System Info ---
